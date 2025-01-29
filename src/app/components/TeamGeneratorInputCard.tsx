c612daf153eec701eb0924d53917c793cc0809cb
Antonio Poloni <antoniopolonijr@gmail.com>
2025-01-29 17:09:06 -0300

    feat(homepage): Create basic structure for home page. Closes#12

---
+++ src/app/components/TeamGeneratorInputCard.tsx
@@ -0,0 +1,5 @@
+import * as React from "react";
+
+export function TeamGeneratorInputCard() {
+  return <div>Team Generator Input Card Component</div>;
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
-import { Button } from "@/components/ui/button";
+import { TeamGeneratorInputCard } from "./components/TeamGeneratorInputCard";
+import { GeneratedTeamsCard } from "./components/GeneratedTeamsCard";
 
 export default function Home() {
   return (
-    <div>
-      <h1>TODO</h1>
-    </div>
+    <main className="container mx-auto py-12 px-2">
+      <h1 className="text-3xl font-bold text-center">NoCliques</h1>
+      <p className="text-center mt-4">
+        NoCliques is a team generator app designed to create fair and balanced
+        teams avoiding the famous "cliques".
+      </p>
+      <div className="flex flex-col gap-6 mt-8 items-center">
+        <TeamGeneratorInputCard />
+        <GeneratedTeamsCard />
+      </div>
+    </main>
   );
 }
